a911b0a513294598bc10cba2dcd05f204719d8b9
Muhammed <muhammedarafa03@gmail.com>
2026-02-17 04:36:12 +0200

    Update query.ts

--- sanity/queries/query.ts
+++ sanity/queries/query.ts
@@ -31,16 +31,31 @@ const BRAND_QUERY = defineQuery(`*[_type == "product" && slug.current == $slug]{
 //   ...,product->
 // }
 // }`);
-const MY_ORDERS_QUERY =
-  defineQuery(`*[_type == 'order' && clerkUserId == $userId] | order(_createdAt desc){
+// const MY_ORDERS_QUERY =
+//   defineQuery(`*[_type == 'order' && clerkUserId == $userId] | order(_createdAt desc){
+//   _id,
+//   orderNumber,
+//   total,
+//   status,
+//   clerkUserId,
+//   _createdAt,
+//   products[] {
+//     ..., 
+//     product->
+//   }
+// }`);
+const MY_ORDERS_QUERY = defineQuery(`
+*[_type == 'order' && clerkUserId == $userId] | order(_createdAt desc){
   _id,
   orderNumber,
   total,
   status,
-  clerkUserId,
   _createdAt,
+  customerName,
+  customerEmail,
+  invoiceNumber,
   products[] {
-    ..., 
+    ...,
     product->
   }
 }`);
